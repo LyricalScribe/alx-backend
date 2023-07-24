3c103ac392e868f6f56eec52c2f00cfeccd5b488
Thulisile Ngomane <ThuleeN@outlook.com>
2023-07-24 23:12:27 +0000

    pagination taks

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that takes two integer arguments"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    return a tuple of size two containing a 
+    start index and an end index
+    """
+    return (((page -1) * page_size), page * page_size)
+
